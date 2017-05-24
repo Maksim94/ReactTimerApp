7adc849de5a9356909f92d1d8910d38b770b62e2
Maxim <maxim.kondzera@gmail.com>
2017-05-24 08:41:29 +0300

    add navigation, fix styles

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -4,14 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Navigation = (props) => {
 	return (
 		<div className="top-bar">
-			<div className="tob-bar-left">
+			<div className="top-bar-left">
 				<ul className="menu">
 			      <li className="menu-text">React Time App</li>
 			      <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
-			      <li><Link to="/" activeClassName="active-link">Countdown</Link></li>
+			      <li><Link to="/countdown" activeClassName="active-link">Countdown</Link></li>
 			    </ul>
 			</div>
-			<div className="tob-bar-right">
+			<div className="top-bar-right">
 				<ul className="menu">
 					<li className="menu-text">
 						Created by <a href="https://github.com/Maksim94">Maxim</a>
